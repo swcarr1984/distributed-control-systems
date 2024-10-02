1b3fd8db0e6219a1e5b12493223e8927ddf2b682
swcarr1984 <66856027+swcarr1984@users.noreply.github.com>
2024-10-02 20:44:36 +0800

    Add files via upload
    Final code used for project testing.

--- controllers/error.js
+++ controllers/error.js
@@ -1,7 +1,7 @@
 // Below is used to direct to the 404 HTML page if an error is encountered during navigation
 
 exports.get404 = (req, res, next) => {
-    res.status(404).render('404', {
+    res.status(404).render('hmi/404', {
    pageTitle: 'Page Not Found',
    path: '/404'
    });
--- controllers/hmi.js
+++ controllers/hmi.js
@@ -1,9 +1,23 @@
 // Below is used to direct to the main HMI landing page for the application
 
+const devices = require('../models/devices'); // imports the class from the models folder
+
+var Device1_PV;
+Device1 = new devices.Device('PIT-210', 'Pressure Transmitter');
+
+/* Allow time for databse to read first client data then call Process Variable 
+   using class method for the model to send to webpage as displayed value */
+setTimeout(() => {
+    Device1_PV =  Device1.fetchPV;
+  }, 15000);
+
+// Render requested page as directed through router with model included
 exports.getIndex = (req, res, next) => {
-        res.render('hmi/index', {
-        pageTitle: 'HMI Landing Page',
-        path: '/'
-        });
-    
-};
+            res.render('hmi/index',{ 
+                devs: Device1,
+                pageTitle: 'HMI Landing Page',
+                path: '/'
+            }); 
+    };
+
+
